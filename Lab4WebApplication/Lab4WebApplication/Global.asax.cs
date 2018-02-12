e4e02fb461e55e2356f32e9ef723b1fd0cb96a84
aalshawa97 <abdullahalshawa@gmail.com>
2018-02-11 16:17:42 -0800

    Exercise 2 Dependency Injection In Progress

--- Lab4WebApplication/Lab4WebApplication/Global.asax.cs
+++ Lab4WebApplication/Lab4WebApplication/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Lab4WebApplication.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -16,7 +17,7 @@ namespace Lab4WebApplication
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            DependencyInjectionConfig.Register();
+            DependecyInjectionConfig.Register();
 
             log4net.Config.XmlConfigurator.Configure();
         }
